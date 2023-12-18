6a73bbafc4ad6ada23bc7ae44a4f0abf2757b5fb
Daniil <daniilminin1990@yandex.ru>
2023-12-18 20:33:20 +0600

    Условный рендеринг

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -10,69 +10,14 @@ type RatingPropsType = {
 // export const Rating = (props: { value: number }) => {
 
 export const Rating = (props: RatingPropsType) => {
-  if (props.value === 1) {
-    return (
-      <div>
-        <Star selected={true} />
-        <Star selected={false} />
-        <Star selected={false} />
-        <Star selected={false} />
-        <Star selected={false} />
-      </div>
-    );
-  }
-  if (props.value === 2) {
-    return (
-      <div>
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={false} />
-        <Star selected={false} />
-        <Star selected={false} />
-      </div>
-    );
-  }
-  if (props.value === 3) {
-    return (
-      <div>
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={false} />
-        <Star selected={false} />
-      </div>
-    );
-  }
-  if (props.value === 4) {
-    return (
-      <div>
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={false} />
-      </div>
-    );
-  }
-  if (props.value === 5) {
-    return (
-      <div>
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={true} />
-        <Star selected={true} />
-      </div>
-    );
-  }
-
+  // Через условнный рендеринг -- т.е. {if props.value === true то }
   return (
     <div>
-      <Star selected={false} />
-      <Star selected={false} />
-      <Star selected={false} />
-      <Star selected={false} />
-      <Star selected={false} />
+      <Star selected={props.value === 1} />
+      <Star selected={props.value === 2} />
+      <Star selected={props.value === 3} />
+      <Star selected={props.value === 4} />
+      <Star selected={props.value === 5} />
     </div>
   );
 };
