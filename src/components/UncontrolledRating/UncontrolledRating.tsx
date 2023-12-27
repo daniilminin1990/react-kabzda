8f1e12895a80873ecc3abc5127086dcd466a8e03
Daniil <daniilminin1990@yandex.ru>
2023-12-27 23:14:21 +0600

    08 - Callback(part1-3 неконтроллируемые компоненты, вариант 2)

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -13,19 +13,18 @@ export const UncontrolledRating = (props: RatingPropsType) => {
   // Через условнный рендеринг -- т.е. {if props.value === true то }
   return (
     <div>
-      <Star selected={value > 0} setValue={setValue} value={1} />
-      <Star selected={value > 1} setValue={setValue} value={2} />
-      <Star selected={value > 2} setValue={setValue} value={3} />
-      <Star selected={value > 3} setValue={setValue} value={4} />
-      <Star selected={value > 4} setValue={setValue} value={5} />
+      <Star selected={value > 0} setValue={() => setValue(1)} />
+      <Star selected={value > 1} setValue={() => setValue(2)} />
+      <Star selected={value > 2} setValue={() => setValue(3)} />
+      <Star selected={value > 3} setValue={() => setValue(4)} />
+      <Star selected={value > 4} setValue={() => setValue(5)} />
     </div>
   );
 };
 
 type StartPropsType = {
   selected: boolean
-  value: 1 | 2 | 3 | 4 | 5
-  setValue: (value: 1 | 2 | 3 | 4 | 5) => void
+  setValue: () => void
 }
 
 function Star(props: StartPropsType) {
@@ -33,5 +32,5 @@ function Star(props: StartPropsType) {
   // Условие. Если selected == true, то отрисуй жирный star
 
   // return props.selected ? <span> <b>star </b> </span> : <span>star </span>
-  return <span onClick={() => props.setValue(props.value)}> {props.selected ? <b>star </b> : 'star '} </span>
+  return <span onClick={() => props.setValue()}> {props.selected ? <b>star </b> : 'star '} </span>
 }
