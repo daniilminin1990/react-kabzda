7cc029aa7ffb46a2ca2335505fec959c918192d1
Daniil <daniilminin1990@yandex.ru>
2024-01-19 01:57:34 +0600

    13 Controlled input, FLUX круговорот, контроллируемые checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useRef, useState } from "react";
+import React, { ChangeEvent, HTMLInputTypeAttribute, useRef, useState } from "react";
 import { action } from "@storybook/addon-actions";
 // import {} from "";
 
@@ -43,4 +43,36 @@ export const GetValueOfUncontrolledInputByButtonPressWithREF = () => {
     </button> - actual value: {value}</>
 };
 
-export const ControlledInput = () => <input value={'it-incubator.by'} />;
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('')
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  return <input value={parentValue} onChange={onChangeHandler} />
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true)
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.checked)
+  }
+  return <input type="checkbox" checked={parentValue} onChange={onChangeHandler} />
+}
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>('1')
+  const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value)
+  }
+  return <select value={parentValue} onChange={onChangeHandler} >
+    <option>none</option>
+    <option value={'1'}>Minsk</option>
+    <option value={'2'}>Moscow</option>
+    <option value={'3'}>Astana</option>
+  </select>
+}
+
+
+
+
+export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'} />;
