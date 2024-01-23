0fce38d51cf21638cf44b3a3862125e00accec30
Daniil <daniilminin1990@yandex.ru>
2024-01-23 18:27:53 +0600

    15 select component - то что в дз в 14 было

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { KeyboardEvent, useEffect, useState } from "react";
+import s from './Select.module.css'
 
 type ItemPropsType = {
   title: string,
@@ -6,22 +7,71 @@ type ItemPropsType = {
 }
 
 type SelectPropsType = {
-  value: any
+  value?: any
   items: ItemPropsType[]
   onChange: (value: any) => void
 };
 
 export const Select = (props: SelectPropsType) => {
+
+  const [active, setActive] = useState(false)
+  const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
+
+  const selectedItem = props.items.find(i => i.value === props.value)
+  const hoveredItem = props.items.find(i => i.value === hoveredElementValue)
+
+  useEffect(() => {
+    setHoveredElementValue(props.value)
+  }, [props.value])
+
+  const toggleItems = () => setActive(!active)
+
+  const onItemClick = (value: any) => {
+    props.onChange(value)
+    setActive(false)
+  }
+
+  const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+      for (let i = 0; i < props.items.length; i++) {
+        if (props.items[i].value === hoveredItem) {
+          const pretendentElement = e.key === 'ArrowDown'
+            ? props.items[i + 1]
+            : props.items[i - 1]
+          if (pretendentElement) {
+            props.onChange(pretendentElement.value)
+            return
+          }
+        }
+      }
+      if (!selectedItem) {
+        props.onChange(props.items[0].value)
+      }
+    }
+    if (e.key === 'Enter' || e.key === 'Escape') {
+      setActive(false)
+    }
+  }
+
+
+
+
   return (
-    <div>
-      {/* 
-      Это div, в которой будет отображаться текущее Value 
-      И если кликаем по div, то должен раскукоживаться список вложенных items, по которому если кликнем, то заменится текущее value на то, по которому кликнули
-      НЕ НУЖНО КОНТРОЛЛИРОВАТЬ ЭТОТ АККОРДЕОН, его раскукоживание и скукоживание, т.е. boolean (collapsed) передавать от родителя не надо
-      Стилизацию взять из MUI componenst = select
-      */}
-      <div>{ }</div>
-      {/* {props.map(i => <div>{i.title}</div>)} */}
-    </div>
+    <>
+      <div className={s.select} onKeyUp={onKeyUp} tabIndex={0} >
+        <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+        {
+          active &&
+          <div className={s.items}>
+            {props.items.map((i) => <div
+              onMouseEnter={() => setHoveredElementValue(i.value)}
+              className={s.item + ' ' + (hoveredItem === i ? s.selected : '')}
+              key={i.value}
+              onClick={() => onItemClick(i.value)}
+            >{i.title}</div>)}
+          </div>
+        }
+      </div>
+    </>
   );
 };
