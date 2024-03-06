714e96bf51b92b5a33c404d8d33be95ecceb24fc
Daniil <daniilminin1990@yandex.ru>
2024-03-06 23:38:32 +0500

    useEffect \ setTimeout \ setInterval Асинхронная логика. ПРОБЛЕМА ЗАМЫКАНИЯ, setState(state => state+1)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react"
 
 export default {
-  title: 'useEffect/ Simple Example',
+  title: 'useEffect demo',
 }
 
+// ЗАПУСКАТЬ НУЖНО iframe
 export const SimpleExample = () => {
   const [fake, setFake] = useState(1)
   const [counter, setCounter] = useState(1);
@@ -36,5 +37,43 @@ export const SimpleExample = () => {
     Hello {counter}
     <button onClick={() => setFake(fake + 1)}>fake+</button>
     <button onClick={() => setCounter(counter + 1)}>+</button>
+  </>
+}
+
+export const SetTimeoutExample = () => {
+  const [fake, setFake] = useState(1)
+  const [counter, setCounter] = useState(1);
+
+  console.log('SetTimeoutExample')
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('setTimeout')
+      document.title = counter.toString()
+    }, 1000)
+  }, [counter]);
+
+  return <>
+    Hello {counter} {fake}
+    <button onClick={() => setFake(fake + 1)}>fake+</button>
+    <button onClick={() => setCounter(counter + 1)}>+</button>
+  </>
+}
+
+export const SetIntervalExample = () => {
+  const [fake, setFake] = useState(1)
+  const [counter, setCounter] = useState(1);
+
+  console.log('SetIntervalExample')
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log('tick: ' + counter)
+      setCounter(state => state + 1)
+    }, 1000)
+  }, []);
+
+  return <>
+    Hello, counter: {counter} - fake: {fake}
   </>
 }
